4dbda51459c8d1182c421ab06aad1f3f43e43bb5
Ayah Kamal <ayahk1999@gmail.com>
2020-08-13 04:14:27 +0300

    added notifs to laundry and option to washing mach

--- src/views/Rooms/LaundryRoom.vue
+++ src/views/Rooms/LaundryRoom.vue
@@ -1,5 +1,87 @@
 <template>
   <div>
+<CAlert class="laundryInfo" color="warning" closeButton>
+   
+
+      <h3 style="margin-bottom: 30px;  background-color: rgba(240, 214, 68, 0.705); color: warning; padding: 5px">Notifications</h3>
+      
+
+      <CRow>
+      <CCard class="laundryNotif" style="margin-left: 10px">
+      <h5 style="margin-bottom: 20px; font-weight: bolder">Current Cycle:</h5>
+      <div class="currentCycle">
+      <p>Washing Machine:</p>
+      <label class="currentCycleLabel">ON</label>
+      </div>
+      <div class="currentCycle">
+      <p>Start Time:</p>
+      <label class="currentCycleLabel">2:10PM</label>
+      </div>
+      <div class="currentCycle">
+      <p>Cycle Mode:</p>
+      <label class="currentCycleLabel">Cotton</label>
+      </div>
+      <div class="currentCycle">
+      <p>Load:</p>
+      <label class="currentCycleLabel">12kg</label>
+      </div>
+      
+
+      <div>
+      <CProgress
+      :value="counter"
+      color="success"
+      animated
+      show-value
+      show-percentage
+      style="height:20px;
+      width: 30vw"
+      class="mt-1"
+    />
+    <label>Time Remaining: 18 Minutes</label>
+    </div>
+
+    </CCard>
+
+
+    <CCard class="laundryNotif" style="margin-left: 10px">
+      <h5 style="margin-bottom: 20px; font-weight: bolder">Weather:</h5>
+      <CCard class="weatherCard">
+        <CRow style="margin-left: 10px">
+        <img src="https://img.icons8.com/cotton/2x/rain.png"
+        style="
+        width: 100px;"/>
+        <CCol>
+        <label>Friday</label>
+        <br>
+        <label>Temperature:</label>
+        <label style="
+        margin-left: 20px;
+        font-weight: bold">
+        29°C</label>
+        <br>
+        <label>Humidity: </label>
+        <label style="
+        margin-left: 43px;
+        font-weight: bold">
+        40%</label>
+        </CCol>
+        </CRow>
+      </CCard>
+
+      <CAlert color="danger" closeButton fade>
+        <b>It is Rainy.</b>
+        <br>
+        Not recommended to dry laundry outside today.
+        <br>
+        <i>Friday 4:11PM</i>
+
+      </CAlert>
+    </CCard>
+     </CRow>
+
+     </CAlert>
+
     <div style="display: flex; justify-content: space-between">
       <h3 class = "device">Devices</h3>
       <button
@@ -60,6 +142,7 @@ export default {
   data () {
     return {
       innerCollapse: false,
+      counter: 86
     }
   }
 }
@@ -67,6 +150,33 @@ export default {
 
 <style>
   @import '../../assets/scss/pageStyle.css';
+
+.laundryInfo{
+  width: 70vw;
+  padding: 20px;
+}
+
+.currentCycle{
+  display: flex;
+}
+
+.currentCycleLabel{
+  margin-left: 10px;
+  font-weight: bold;
+}
+
+.laundryNotif{
+  width: 31vw;
+  background-color: rgb(240, 239, 239);
+  border-radius: 20px;
+  padding: 10px;
+}
+
+.weatherCard{
+  border-radius: 20px;
+  padding: 5px;
+}
+
 
 
 </style>
--- src/views/devices/washingMachine.vue
+++ src/views/devices/washingMachine.vue
@@ -2,8 +2,56 @@
     <CRow class="rowStyle">
         <CCard>
           <h4 class = "title">Washing Machine</h4>
+
+           <Button  class="optBtn"
+          @click="innerCollapse = !innerCollapse; showMessage()" 
+          size="sm" 
+          color="secondary"> 
+          <CIcon name="cil-settings" style="margin-right: 5px"/>
+         More Options
+        </Button>
+
         <img  class = "deviceImage" src="https://devicebase.net/products/218/teaser-lg.jpg" alt="">
-  
+   
+   <CCollapse :show="innerCollapse" class="mt-2">
+          <CCard body-wrapper>
+
+            <h4>More Options:</h4>
+            <hr>
+            <h5 style="margin-left: 5px; margin-bottom: 10px">Set Wash Cycle:</h5>
+
+
+<label class = "editLabel">Start Time:</label>
+<input type="time" style="margin-left: 15px" required>
+
+<hr>
+
+<label class = "editLabel" for="customRange2">Cycle:</label>
+<br>
+<div style="display: grid">
+<div class="btn-group" role="group" aria-label="Basic example">
+  <button type="radio" class="buttons">Cotton</button>
+  <button type="radio" class="buttons">Speed Wash</button>
+  <button type="radio" class="buttons">Sports Wear</button>
+</div>
+<br>
+<div class="btn-group" role="group" aria-label="Basic example">
+  <button type="radio" class="buttons">Wool</button>
+  <button type="radio" class="buttons">Rinse+Spin</button>
+  <button type="radio" class="buttons">Bedding Care</button>
+</div>
+<br>
+<div class="btn-group" role="group" aria-label="Basic example">
+  <button type="radio" class="buttons">Baby Care</button>
+   <button type="radio" class="buttons">Duvet</button>
+    <button type="radio" class="buttons">Single Shirt</button>
+</div>
+</div>
+
+
+          </CCard>
+        </CCollapse>
+
    <div class="onoffswitch">
     <input type="checkbox" name="onoffswitch" class="onoffswitch-checkbox" id="myonoffswitch4" tabindex="0" checked>
     <label class="onoffswitch-label" for="myonoffswitch4">
@@ -16,3 +64,76 @@
 </CCard>
 </CRow>
 </template>
+
+<script>
+export default {
+    data () {
+    return {
+      innerCollapse: false,
+    }
+},
+ mounted(){
+   $('.optBtn').click(function(){
+  $(this).toggleClass('green');
+
+  $('.buttons').click(function(){
+  $(".buttons").removeClass("blue");
+    $(this).addClass("blue");
+});
+});
+  },
+
+}
+</script>
+
+<style scoped>
+.optBtn {
+  background-color:#4F7FBF;
+  color: white;
+  text-align: center;
+  display: inline-block;
+  font-size: 10px;
+  border: none;
+}
+
+.green{
+  background-color:rgb(54, 107, 72);
+  }
+
+
+.buttons{
+    border: none;
+    background-color: rgb(216, 216, 216);
+    padding: 5px;
+    padding-left: 10px;
+    padding-right: 10px;
+    border-radius: 20px;
+    margin-right: 5px;
+}
+
+.blue{
+    background-color: rgb(91, 150, 238);
+}
+ 
+.editLabel{
+    margin-left: 5px;
+    margin-right :5px;
+    margin-top:10px;
+  font-family: Trebuchet, Arial, sans-serif; font-weight: bold;
+  font-size: 15px;
+  text-align: center;
+  margin-top: 7px;
+}
+
+.modeLabel{
+  margin-left: 5px;
+    margin-right :5px;
+    margin-top:10px;
+  font-family: Trebuchet, Arial, sans-serif; font-weight: bold;
+  font-size: 14px;
+  text-align: center;
+  margin-top: 7px;
+  color: #4F7FBF;
+}
+
+</style>
