8f00415de9632905518c5e611d04031a1afe345f
Ayah Kamal <ayahk1999@gmail.com>
2020-08-20 01:42:48 +0300

    finished overview and notifications on dahsboard

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -2,6 +2,7 @@
   <div>
     <welcomeMessage/>
 
+   <div class="updatesContainer">
    <div  id = "slide">
    <CCarousel
     arrows
@@ -35,6 +36,54 @@
     />
   </CCarousel>
 </div>
+
+<CAlert color="warning"
+    style="width: 450px; height: 250px">
+    <h3 style="color: black">Notifications</h3>
+    <CIcon name= "cil-bell-exclamation" style="float: left; margin-right: 5px;"/>
+    Monthly report for July: 
+     <CLink  class="text-white bg-warning px-1"
+       style="font-size: 13px;
+       margin-left: 5px;">
+         July Monthly Report</CLink>
+    <br>
+    
+    <CIcon name= "cil-bell-exclamation" style="float: left; margin-right: 5px;"/>
+    <i>'Air Conditioning'</i> settings in <i><b>Master Bedroom</b></i> changed to: 
+     <label style="margin-left: 19px; margin-bottom: -1px">Cool Medium Fan - 23°C</label>
+   <br>
+   
+    <CIcon name= "cil-bell-exclamation" style="float: left; margin-right: 5px;"/>
+    Hourly report for Wed 19 August: 
+     <CLink  class="text-white bg-warning px-1"
+       @click='routeToSettings()'
+       style="font-size: 13px;
+       margin-left: 5px;">
+         19/08/2020 Hourly Report</CLink>
+    <br>
+
+    <CIcon name= "cil-bell-exclamation" style="float: left; margin-right: 5px;"/>
+    <i>'Smoke Detector'</i> running out of battery, add <u>AA batteries</u> to  
+    <label style="margin-left: 20px; margin-bottom: -1px">shopping list?</label>
+    <br>
+
+   <div style="background-color: #FFC63D; margin-bottom: -15px">
+    <CIcon name= "cil-bell-exclamation" style="float: left; margin-right: 5px;"/>
+    <i>'Washing Machine'</i> exceeded set power limit for month. Do 
+    <label style="margin-left: 20px; margin-bottom: -1px">
+      you want to change appliance status?</label> 
+      <CLink style="margin-left: 85px; margin-right: 5px">✔</CLink> <CLink>❌</CLink></div>
+    <br> 
+
+    <CIcon name= "cil-bell-exclamation" style="float: left; margin-right: 5px;"/>
+    New device <i>'Dishwasher'</i> added to <i><b>Kitchen</b></i>
+    <br>  
+
+     
+
+  </CAlert>
+  </div>
+
 <br>
 
 <h4>Overview</h4>
@@ -43,7 +92,7 @@
 <div class="energyContainer">
 <CChartLine
     style="height:350px;
-    width: 480px"
+    justify-content: center"
     :datasets="[
       {
         data: [600, 610, 654, 697, 700, 706, 793, , , , , , ,],
@@ -140,6 +189,14 @@ export default {
 
 
 <style scoped>
+.updatesContainer{
+display: flex;
+justify-content: space-evenly;
+flex-wrap: wrap;
+margin-top: 20px;
+gap: 10px;
+}
+
 .slide{
   width: 450px;
   height: 250px;
@@ -173,12 +230,19 @@ h5{
 }
 
 .overviewContainer{
-  display: flex;
+  /* display: flex;
   flex-wrap: wrap;
   flex-direction: row;
-  justify-content: space-evenly;
+  justify-content: space-evenly;*/
   padding: 5px;
-  margin-bottom: 20px;
+  margin-bottom: 20px; 
+
+  display: grid;
+  grid-template-columns: 8fr 4fr;
+  grid-template-rows: auto;
+  gap: 2rem;
+
+
 }
 
 .energyContainer{
@@ -190,7 +254,7 @@ background-color: #dee8f0;
 
 .monthEnergyWidget{
   min-width: 15vw;
-  max-width: 17.5vw;
+  max-width: 35vw; 
   max-height: 250px;
   background-color: rgb(195, 220, 236);
   margin-left: 10px;
@@ -200,8 +264,31 @@ background-color: #dee8f0;
 
 .monthEnergyContainer{
   margin-top:20px;
-  display: flex;
-  flex-flow: column wrap;
+  /* display: flex;
+  flex-flow: column wrap; */
+
+  display: grid;
+  grid-template-columns: 1fr;
+  grid-template-rows: 1fr 1fr;
+  gap: 0.5rem;
+}
+
+.monthEnergyContainer .monthEnergyWidget{
+  height: 200px;
+}
+
+@media (max-width: 720px) {
+  .overviewContainer{
+    grid-template-columns: 1fr;
+    grid-template-rows: auto auto;
+  }
+
+  .monthEnergyContainer {
+    grid-template-columns: 1fr 1fr;
+    grid-template-rows: 1fr;
+  
+  }
+  
 }
 
 
