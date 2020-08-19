e61247bdb38d6dd1b070fd917417a99731a0905f
Ayah Kamal <ayahk1999@gmail.com>
2020-08-19 21:39:34 +0300

    added overview and removed status

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -8,84 +8,89 @@
     indicators
     animate = true
     interval = "6000"
-    height="350px"
+    height="100%"
   >
     <CCarouselItem
       captionHeader="Home"
       image="https://images.adsttc.com/media/images/5be9/fd5c/08a5/e5a5/8c00/008f/large_jpg/CARLES_FAUS_ARQUITECTURA_-_CARMEN_HOUSE_(2).jpg?1542061390"
-      style="width:550px; height:350px;"
+      style="width:450px; height:250px;"
     />
     <CCarouselItem
       captionHeader="Living Room"
       image="https://media.architecturaldigest.com/photos/560c37dd7da26e3235ad995e/16:9/w_1280,c_limit/gray-living-room-01.jpg"
-      style="width:550px; height:350px;"
+      style="width:450px; height:250px;;"
       captionText = "6 Active Devices"
     />
     <CCarouselItem
       captionHeader="Kitchen"
       image="https://hgtvhome.sndimg.com/content/dam/images/hgtv/fullset/2018/4/23/1/HUHH2018-Curb-Appeal_Seattle-WA_11.jpg.rend.hgtvcom.966.644.suffix/1524514638493.jpeg"
-      style="width:550px; height:350px;"
+      style="width:450px; height:250px;"
       captionText = "13 Active Devices"
     />
     <CCarouselItem
      captionHeader="Master Bedroom"
      image="https://assets.blog.hgtv.ca/wp-content/uploads/2019/10/28123144/celeb-bedroom-justin-bieber2.jpg"
-     style="width:550px; height:350px;"
+     style="width:450px; height:250px;"
      captionText = "6 Active Devices"
     />
   </CCarousel>
 </div>
 <br>
 
-<h4>Status</h4>
-<div>
-  <div class="wrapper cards">
-  <div class="container">
-    <div class="card">
-      <div class="front">
-        <img class="status" src="https://cdn3.iconfinder.com/data/icons/domestic-2/68/door_open_entrance_wall_enter_2-512.png"/>
-        <h5>Back Home</h5>
-        </div>
-      <div class="back">
-          <img class="status" src="https://cdn3.iconfinder.com/data/icons/domestic-2/68/door_open_entrance_wall_enter_exit-512.png"/>
-          <h5>Away Home</h5>
-      </div>
-    </div>
+<h4>Overview</h4>
+<div class="overviewContainer">
+  
+<div class="energyContainer">
+<CChartLine
+    style="height:350px;
+    width: 480px"
+    :datasets="[
+      {
+        data: [600, 610, 654, 697, 700, 706, 793, , , , , , ,],
+        fill: false,
+        borderColor: 'rgb(235, 182, 36)',
+        label: 'Energy Consumption Level',
+      },
+      {
+        data: [ , , , , , ,793,800, 850, 790, 750, 700, 690],
+         fill: false,
+         borderColor: 'lightblue',
+         borderDash: [10],
+        label: 'Predicted Energy Consumption Level',
+      }
+    ]"
+    labels="months"
+    :options="{ maintainAspectRatio: false }"
+  />
+   <h7>Year: 2020</h7>
   </div>
+
+<div class="monthEnergyContainer">
+  <CWidgetProgress
+        class="monthEnergyWidget"
+        header="July"
+        text="Limit Set: 800kWh"
+        footer="Power Usage: 793kWh"
+        color="secondary"
+        :value="89"
+      />
+      <CWidgetProgress
+        class="monthEnergyWidget"
+        header="June"
+        text="Limit Set: 795kWh"
+        footer="Power Usage: 706kWh"
+        color="secondary"
+        :value="75"
+      />
+   </div>
+
 </div>
 
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
 <h4></h4>
 <br>
 
 <weatherWidget/>
-
-
 </div>
-
-<!-- <CElementCover 
-        :boundaries="[{ sides: ['top', 'left'], query: '.media-body' }]"
-        :opacity="0.8" :center = true
-      >
-        <h1 class="d-inline">Loading... </h1><CSpinner size="5xl" color="success"/>
-      </CElementCover> -->
-    </div>
-
 </template>
 
 
@@ -136,8 +141,8 @@ export default {
 
 <style scoped>
 .slide{
-  width: 550px;
-  height: 355px;
+  width: 450px;
+  height: 250px;
   display: block;
   margin-left: auto;
   margin-right: auto;
@@ -152,6 +157,7 @@ h4{
     text-align: center;
     padding: 10px;
     background-color: rgb(82, 110, 163);
+    margin-bottom: 0;
 }
 
 h5{
@@ -166,172 +172,38 @@ h5{
   height: 100px;
 }
 
-@import url(https://fonts.googleapis.com/css?family=Roboto+Slab:400,100,300,700);
-
-body {
-  font-family: 'Roboto Slab', serif;
-  font-weight: 300;
-  font-size: 16px;
-  line-height: 1.4em;
-  color: #333;
-  background: #eee;
+.overviewContainer{
+  display: flex;
+  flex-wrap: wrap;
+  flex-direction: row;
+  justify-content: space-evenly;
+  padding: 5px;
+  margin-bottom: 20px;
 }
 
-input,
-button {
-  font-family: 'Roboto Slab', serif;
-  font-weight: 300;
-  font-size: 16px;
-  border: 0;
-  padding: 3px 5px;
-  border-radius: 3px;
+.energyContainer{
+padding: 10px;
+height: 43vh;
+margin-top: 20px;
+background-color: #dee8f0;
 }
 
-h1 {
-  margin: 0.5em 0 1em 0;
-  font-size: 1.8em;
-  font-weight: 700;
-  color: #096AA3;
-}
-
-h2, h3 {
-  font-weight: bold;
-}
-
-p {
-  margin-bottom: 1em;
-}
-
-.animation {
-  -webkit-transition: all 0.3s ease;
-  -moz-transition: all 0.3s ease;
-  -ms-transition: all 0.3s ease;
-  -o-transition: all 0.3s ease;
-  transition: all 0.3s ease;
-}
-
-.wrapper {
-  width: 80%;
-  padding: 4%;
-  margin: 20px auto;
-  background: #fff;
-}
-
-.wrapper.cards {
-  background: 0;
-  width: 88%;
-  padding: 20px 0 0 0;
-}
-
-.container {
-  position: relative;
-  float: left;
-  width: 48%;
-  height: 260px;
-  margin: 10px 0 10px 4%;
-  background: rgba(255, 255, 255, 0);
-  
-  
-  /* Set the depth of the elements */
-  -webkit-perspective: 800px;
-  -moz-perspective: 800px;
-  -o-perspective: 800px;
-  perspective: 800px;
-}
-
-.container:first-child {
-  margin-left: 0;
-}
-
-.card {
-  width: 100%;
-  height: 100%;
-  position: center;
-  cursor: pointer;
-
-    left: 55%;
-
-  /* Set the transition effects */
-  -webkit-transition: -webkit-transform 0.4s;
-  -moz-transition: -moz-transform 0.4s;
-  -o-transition: -o-transform 0.4s;
-  transition: transform 0.4s;
-  -webkit-transform-style: preserve-3d;
-  -moz-transform-style: preserve-3d;
-  -o-transform-style: preserve-3d;
-  transform-style: preserve-3d;
-}
-
-.card.flipped {
-  -webkit-transform: rotateY( 180deg );
-  -moz-transform: rotateY( 180deg );
-  -o-transform: rotateY( 180deg );
-  transform: rotateY( 180deg );
-}
-
-.card .front,
-.card .back {
-  display: block;
-  height: 100%;
-  width: 100%;
-  line-height: 260px;
-  color: white;
-  text-align: center;
-  font-size: 4em;
-  position: absolute;
-  -webkit-backface-visibility: hidden;
-  -moz-backface-visibility: hidden;
-  -o-backface-visibility: hidden;
-  backface-visibility: hidden;
-  
-  box-shadow: 3px 5px 20px 2px rgba(0, 0, 0, 0.25);
+.monthEnergyWidget{
+  min-width: 15vw;
+  max-width: 17.5vw;
+  max-height: 250px;
+  background-color: rgb(195, 220, 236);
+  margin-left: 10px;
+  margin-right: 10px;
 }
 
 
-.card .front {
-  background: #222222;
+.monthEnergyContainer{
+  margin-top:20px;
+  display: flex;
+  flex-flow: column wrap;
 }
 
-.card .back {
-  background: #444444;
-  -webkit-transform: rotateY( 180deg );
-  -moz-transform: rotateY( 180deg );
-  -o-transform: rotateY( 180deg );
-  transform: rotateY( 180deg );
-}
 
-.container:first-child .card .front {
-  background: #94999c;
-}
-
-.container:first-child .card .back {
-  background: #b8bfc4;
-}
-
-.cardTitle {
-  font-size: 1.4em;
-  line-height: 1.2em;
-  margin: 0;
-}
-
-.content {
-  padding: 4%;
-  font-weight: 100;
-  text-align: left;
-}
-
-button.btnSend {
-  display: inline-block;
-  min-width: 100px;
-  padding: 3px 5px;
-  margin-top: 10px;
-  font-weight: bold;
-  text-transform: uppercase;
-  text-align: center;
-  color: #03446A;
-  background: #fff;
-  border: 0;
-  border-radius: 3;
-}
 
 </style>
